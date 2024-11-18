234d9d67441c771564fb437af89264d1bb43a6ba
RIFDAN123 <ripdan936@gmail.com>
2024-11-18 17:19:16 +0800

    Create tugas7.py

---
+++ tugas7.py
@@ -0,0 +1,9 @@
+# Menghitung luas persegi
+# nama : Rifdan
+
+print("menghitung luas persegi  ")
+panjang = int(input("Masukkan panjang : "))
+lebar = int(input("Masukan lebar : "))
+
+hasil = panjang * lebar 
+print(f"Hasil dari {panjang } x {lebar} = {hasil}")
